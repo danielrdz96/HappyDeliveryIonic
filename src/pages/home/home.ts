273c34789176abd9d077488e65c90e526de62061
unknown <daniel_rodriguez_46@hotmail.com>
2017-10-02 12:00:32 -0500

    Login

---
+++ src/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { RegisterPage } from '../registrarse/registro';
+
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+  Registrarse () {
+    this.navCtrl.push(RegisterPage);
+  }
+
+}
